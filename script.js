48b6f34849cea92f10c9192deb28fdcda319e80e
thomaz antiszko <140639597+thomazlindao@users.noreply.github.com>
2025-09-15 09:37:04 -0300

    fff

--- script.js
+++ script.js
@@ -59,5 +59,5 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add ('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha";
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha";
 }
